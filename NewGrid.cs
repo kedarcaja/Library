d1f39a02e48ab98b4203739d3ce0581d101a947c
kedarcaja <kedarcaja@gmail.com>
2017-10-25 19:30:40 +0200

    Update NewGrid.cs

--- NewGrid.cs
+++ NewGrid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class NewBehaviourScript : MonoBehaviour {
+public class NewGrid : MonoBehaviour {
     public float snapInterval = 0.7f;
     private void Update()
     {
