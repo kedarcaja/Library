37014446a31418f918fb7e7ca61e66687b18ac6f
kedarcaja <kedarcaja@gmail.com>
2017-10-25 19:29:42 +0200

    NewGrid

---
+++ NewGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class NewBehaviourScript : MonoBehaviour {
+    public float snapInterval = 0.7f;
+    private void Update()
+    {
+        Transform[] children = transform.GetComponentInChildren<Transform>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i].transform.position = RoundVector(children[i].transform.position,snapInterval);
+        }
+    }
+
+    float Round(float number, float interval) {
+        return(float)((int)(number/interval))* interval;
+    }
+
+    Vector3 RoundVector(Vector3 vector, float interval)
+    {
+        return new Vector3(Round(vector.x, interval), Round(vector.y, interval), Round(vector.z, interval));
+    }
+}
