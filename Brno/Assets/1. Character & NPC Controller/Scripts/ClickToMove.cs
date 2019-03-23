b20b3f430904fb32b53d0ab39f465f801e955eb2
Michal <sobisis@gmail.com>
2019-03-23 01:50:18 +0100

    base
    Design

---
+++ Brno/Assets/1. Character & NPC Controller/Scripts/ClickToMove.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+// Use physics raycast hit from mouse click to set agent destination
+[RequireComponent(typeof(NavMeshAgent))]
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+
+    private float Range = 5f;
+    RaycastHit m_HitInfo = new RaycastHit();
+
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+                m_Agent.destination = m_HitInfo.point;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
