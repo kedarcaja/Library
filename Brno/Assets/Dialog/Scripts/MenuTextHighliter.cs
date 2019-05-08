5e93b2cd506355189f52fef2368c4ffaef524a72
vitekd5 <30870070+vitekd5@users.noreply.github.com>
2019-05-09 01:14:51 +0200

    text highliter

---
+++ Brno/Assets/Dialog/Scripts/MenuTextHighliter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MenuTextHighliter : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> items = new List<TextMeshProUGUI>();
+    private int index = 0, maxIndex, minIndex;
+    void Awake()
+    {
+        minIndex = 0;
+        maxIndex = items.Count;
+        index = 0;
+        Highlite(Color.yellow, items[index]);
+    }
+    public int  GetSelectedIndex()
+    {
+        return index;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (index < maxIndex)
+            {
+                index++;
+            }
+            else if (index == maxIndex)
+            {
+                index = minIndex;
+            }
+            Highlite(Color.yellow, items[index]);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (index > minIndex)
+            {
+                index--;
+            }
+            else if (index == minIndex)
+            {
+                index = maxIndex;
+            }
+            Highlite(Color.yellow, items[index]);
+
+        }
+
+    }
+    private void Highlite(Color col, TextMeshProUGUI item)
+    {
+        foreach (TextMeshProUGUI t in items)
+        {
+            t.color = Color.white;
+        }
+        item.color = col;
+        index = items.IndexOf(item);
+    }
+    public void SelectItem(TextMeshProUGUI t)
+    {
+        Highlite(Color.yellow, t);
+    }
+}
