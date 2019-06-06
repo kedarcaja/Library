6d9465548136326bbe86e7655b60b5e4504f6de3
vitekd55 <37591110+vitekd55@users.noreply.github.com>
2019-06-06 12:03:16 +0200

    grid

--- Brno/Assets/BehaviourEditor/BehaviourEditor.cs
+++ Brno/Assets/BehaviourEditor/BehaviourEditor.cs
@@ -32,9 +32,10 @@ namespace BehaviourTreeEditor
         Vector2 scrollPos;
         Vector2 scrollStartPos;
         public GUIStyle style;
-
-        #endregion
-        public enum UserActions
+		private Vector2 offset;
+		private Vector2 drag;
+		#endregion
+		public enum UserActions
         {
             deleteNode, commentNode, stateNode, makeTransition, conditionNode
         }
@@ -48,7 +49,10 @@ namespace BehaviourTreeEditor
         private void OnGUI()
         {
 
-            Event e = Event.current;
+			DrawGrid(20, 0.2f, Color.gray);
+			DrawGrid(100, 0.4f, Color.gray);
+
+			Event e = Event.current;
             mousePosition = e.mousePosition;
             HandleZoom(e);
             UserInput(e);
@@ -113,7 +117,31 @@ namespace BehaviourTreeEditor
             menu.ShowAsContext();
             e.Use();
         }
-        private void UserInput(Event e)
+		private void DrawGrid(float gridSpacing, float gridOpacity, Color gridColor)
+		{
+			int widthDivs = Mathf.CeilToInt(position.width / gridSpacing);
+			int heightDivs = Mathf.CeilToInt(position.height / gridSpacing);
+
+			Handles.BeginGUI();
+			Handles.color = new Color(gridColor.r, gridColor.g, gridColor.b, gridOpacity);
+
+			offset += drag * 0.5f;
+			Vector3 newOffset = new Vector3(offset.x % gridSpacing, offset.y % gridSpacing, 0);
+
+			for (int i = 0; i < widthDivs; i++)
+			{
+				Handles.DrawLine(new Vector3(gridSpacing * i, -gridSpacing, 0) + newOffset, new Vector3(gridSpacing * i, position.height, 0f) + newOffset);
+			}
+
+			for (int j = 0; j < heightDivs; j++)
+			{
+				Handles.DrawLine(new Vector3(-gridSpacing, gridSpacing * j, 0) + newOffset, new Vector3(position.width, gridSpacing * j, 0f) + newOffset);
+			}
+
+			Handles.color = Color.white;
+			Handles.EndGUI();
+		}
+		private void UserInput(Event e)
         {
             if (currentGraph == null) return;
             clickedOnWindow = false;
