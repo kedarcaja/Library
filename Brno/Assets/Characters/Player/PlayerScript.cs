58d45c0e2c292dbf1ebcac99e9153f2db569982c
vitekd55 <37591110+vitekd55@users.noreply.github.com>
2019-06-18 13:23:18 +0200

    player move

--- Brno/Assets/Characters/Player/PlayerScript.cs
+++ Brno/Assets/Characters/Player/PlayerScript.cs
@@ -1,38 +1,58 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerScript : CharacterScript
 {
-	private Vector3 previousPosition;
+	private Vector3 previousPosition, moveVector;
 	public float curSpeed;
-	
+	public float rotateSpeed;
+	private float magnditude = 0;
+	private const float  maxMagnitude = 1;
 
+
+
+	private void Start()
+	{
+	}
 	protected override void Update()
 	{
-
-		anim.SetFloat("speed",GetAgentVelocityManditude());
-
+		if (magnditude > maxMagnitude) magnditude = maxMagnitude;
 
 		if (Input.GetKey(KeyCode.LeftShift))
 		{
-			agent.speed = 5;
+			agent.speed = 5f;
+			anim.SetFloat("magnitudeSpeed", magnditude , 0.0f, Time.deltaTime);
+
+
 		}
 		else
 		{
-			agent.speed = 2.25f;
+			agent.speed = 2.5f;
+			anim.SetFloat("magnitudeSpeed", magnditude / 2, 0.0f, Time.deltaTime);
+
+
 		}
-	}
 
 
 
-	private float GetAgentVelocityManditude()
-	{
-		Vector3 curMove = transform.position - previousPosition;
-		curSpeed = curMove.magnitude / Time.deltaTime;
-		previousPosition = transform.position;
-		return curSpeed;
+		moveVector.x = Input.GetAxis("Horizontal");
+		moveVector.z = Input.GetAxis("Vertical");
+
+		magnditude = moveVector.sqrMagnitude;
+
+
+
+		agent.Move(moveVector * agent.speed * Time.deltaTime);
+
+		if (moveVector != Vector3.zero)
+		{
+			agent.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveVector), rotateSpeed);
+
+		}
+
 	}
 }
 
